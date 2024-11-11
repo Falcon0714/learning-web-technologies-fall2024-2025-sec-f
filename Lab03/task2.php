8862e98d16e871214b7515374179668732bd2327
Falcon0714 <tanvirjabid@gmail.com>
2024-11-11 16:15:05 +0600

    Delete Lab03 directory

--- Lab03/task2.php
+++
@@ -1,7 +0,0 @@
-<?php
-$amount = 100;
-$vat = 0.15 * $amount;
-$total = $amount+ $vat;
-echo "Total Amount (VAT inc.): $total<br>";
-echo "\nVAT: $vat\n";
-?>
--- Lab03/task3.php
+++
@@ -1,9 +0,0 @@
-<?php
-$number = 7;
-
-if ($number % 2 == 0) {
-    echo "$number is even\n";
-} else {
-    echo "$number is odd\n";
-}
-?>
